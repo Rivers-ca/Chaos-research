17c749c6757153b19dcc43e99459a63dfc636470
Rivers Calareso <riverscalareso@gmail.com>
2026-08-11 18:48:09 -0700

    Restructure repository for Chaos Research

--- Math-Nerdy-Stuff/chi_square_weaker.py
+++
@@ -1,8 +0,0 @@
-from scipy.stats import chisquare
-
-observed = [139, 116, 84, 92]
-total = sum(observed)  # 431
-expected = [total/4] * 4  # [107.75, 107.75, 107.75, 107.75]
-
-stat, p = chisquare(observed, f_exp=expected)
-print(f"chi2 = {stat:.3f}, p = {p:.4f}")
